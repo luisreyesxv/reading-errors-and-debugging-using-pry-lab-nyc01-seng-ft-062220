e8e46ca36cacbfe9af30337d83c033d41b326550
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-07 03:47:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
+require "pry"
 
 def snake_it_up(string)
   if string[0] == "s"
-    do "s" + string 10.times end
+    "s" + string
+    binding.pry
   else
     string
   end
