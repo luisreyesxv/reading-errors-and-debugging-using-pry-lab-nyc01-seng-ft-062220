7cb619727cb984c9bcb9afba8e3559f4934bfcd1
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-07 03:40:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,7 @@
 # don't forget to add: require 'pry'
 
+require "pry"
+
 def get_user_input
   gets.chomp
 end
@@ -13,6 +15,8 @@ end
 def selection(num)
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
+     binding.pry
+
   elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
